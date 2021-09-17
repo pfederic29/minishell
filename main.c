8d51caa0e632b93e3904a12dc935087db7ab0a84
Paolo Federici <pfederic@e3r8p3.42roma.it>
2021-09-17 16:11:24 +0200

    readline non funziona

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "minishell.h"
+
+void ft_loop()
+{
+	const char *line;
+	char **args;
+	int status;
+
+	while (status)
+	{
+		line = readline();
+	}
+	
+}
+
+int main()
+{
+	ft_loop();
+	return (1);
+}
---
+++ minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void ft_loop();
+
+#endif
+
